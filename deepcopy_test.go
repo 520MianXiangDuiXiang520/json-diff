02e37dd40b18dadb19c08507d0ed7b84f043e1b0
520MianXiangDuiXiang520 <15364968962@163.com>
2021-05-15 22:17:06 +0800

    添加深拷贝速率测试结果

--- deepcopy_test.go
+++ deepcopy_test.go
@@ -1,8 +1,14 @@
 package json_diff
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"runtime/pprof"
 	"testing"
+	"time"
 )
 
 func TestDeepCopy_emptyObject(t *testing.T) {
@@ -22,7 +28,7 @@ func TestDeepCopy_emptyObject(t *testing.T) {
 }
 
 func TestDeepCopy(t *testing.T) {
-	fileName := "./test_data/deepcopy_test.json"
+	fileName := "./test_data/deepcopy_test/deepcopy_test.json"
 	input, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		t.Error("fail to open the ", fileName)
@@ -68,4 +74,61 @@ func TestDeepCopy(t *testing.T) {
 	if of.Value.(string) != "jjj" {
 		t.Errorf("change source object: %v", cpof.Value)
 	}
+}
+
+func oldDeepCopy(dst, src interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
+}
+
+func TestDeepCopy_speed(t *testing.T) {
+	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Error("fail to open the ", fileName)
+	}
+	srcNode, _ := Unmarshal(input)
+	deepCopySpeedTestHelper(srcNode, t, 1000, true, true)
+	deepCopySpeedTestHelper(srcNode, t, 1000, true, false)
+	deepCopySpeedTestHelper(srcNode, t, 1000, false, true)
+}
+
+func deepCopySpeedTestHelper(srcNode *JsonNode, t *testing.T, loop int, doOld, doNew bool) {
+	path := "test_data/deepcopy_test/pprof_result"
+	name := ""
+	var err error
+	if doOld && doNew {
+		name = fmt.Sprintf("compared_cpu_profile_%d", time.Now().UnixNano())
+	} else if doOld {
+		name = fmt.Sprintf("old_func_cpu_profile_%d", time.Now().UnixNano())
+	} else if doNew {
+		name = fmt.Sprintf("new_func_cpu_profile_%d", time.Now().UnixNano())
+	}
+	f, err := os.Create(fmt.Sprintf("%s/%s", path, name))
+	if err != nil {
+		t.Errorf("can not Start CPU Profile: %v", err)
+	}
+	_ = pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+	startTime := time.Now().UnixNano()
+	for i := 0; i < loop; i++ {
+		if doOld {
+			oldCopyRes := new(JsonNode)
+			err = oldDeepCopy(oldCopyRes, srcNode)
+			if err != nil {
+				t.Errorf("fail to copy by old function: %v", err)
+			}
+		}
+		if doNew {
+			_, err = DeepCopy(srcNode)
+			if err != nil {
+				t.Errorf("fail to copy by new function: %v", err)
+			}
+		}
+	}
+	spend := (time.Now().UnixNano() - startTime) / 1000000
+	fmt.Printf("do %d loops spend %v ms \n", loop, spend)
 }
