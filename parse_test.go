64925515658089d143c76e850f6544c4e24d0096
520MianXiangDuiXiang520 <15364968962@163.com>
2021-08-21 11:01:52 +0800

    恢复case

--- parse_test.go
+++ parse_test.go
@@ -3,7 +3,9 @@ package json_diff
 import (
 	"fmt"
 	"github.com/520MianXiangDuiXiang520/json-diff/decode"
-	// "io/ioutil"
+    `io/ioutil`
+    
+    // "io/ioutil"
 	"testing"
 )
 
@@ -15,56 +17,56 @@ func TestUnmarshal(t *testing.T) {
 	fmt.Println(node, err)
 }
 
-// func BenchmarkMarshal(b *testing.B) {
-// 	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
-// 	// fileName := "./test_data/hash_test.json"
-// 	input, err := ioutil.ReadFile(fileName)
-// 	if err != nil {
-// 		b.Error("fail to open the ", fileName)
-// 	}
-// 	node, _ := decode.Unmarshal(input)
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		_, _ = decode.Marshal(node)
-// 	}
-// }
-//
-// func BenchmarkMarshalOld(b *testing.B) {
-// 	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
-// 	// fileName := "./test_data/hash_test.json"
-// 	input, err := ioutil.ReadFile(fileName)
-// 	if err != nil {
-// 		b.Error("fail to open the ", fileName)
-// 	}
-// 	node, _ := Unmarshal(input)
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		_, _ = Marshal(node)
-// 	}
-// }
-//
-// func BenchmarkUnmarshal(b *testing.B) {
-// 	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
-// 	// fileName := "./test_data/hash_test.json"
-// 	input, err := ioutil.ReadFile(fileName)
-// 	if err != nil {
-// 		b.Error("fail to open the ", fileName)
-// 	}
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		_, _ = decode.Unmarshal(input)
-// 	}
-// }
-//
-// func BenchmarkUnmarshalOld(b *testing.B) {
-// 	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
-// 	// fileName := "./test_data/hash_test.json"
-// 	input, err := ioutil.ReadFile(fileName)
-// 	if err != nil {
-// 		b.Error("fail to open the ", fileName)
-// 	}
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		_, _ = Unmarshal(input)
-// 	}
-// }
+func BenchmarkMarshal(b *testing.B) {
+	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
+	// fileName := "./test_data/hash_test.json"
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		b.Error("fail to open the ", fileName)
+	}
+	node, _ := decode.Unmarshal(input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = decode.Marshal(node)
+	}
+}
+
+func BenchmarkMarshalOld(b *testing.B) {
+	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
+	// fileName := "./test_data/hash_test.json"
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		b.Error("fail to open the ", fileName)
+	}
+	node, _ := Unmarshal(input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Marshal(node)
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
+	// fileName := "./test_data/hash_test.json"
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		b.Error("fail to open the ", fileName)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = decode.Unmarshal(input)
+	}
+}
+
+func BenchmarkUnmarshalOld(b *testing.B) {
+	fileName := "./test_data/deepcopy_test/deepcopy_speed_test.json"
+	// fileName := "./test_data/hash_test.json"
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		b.Error("fail to open the ", fileName)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Unmarshal(input)
+	}
+}
